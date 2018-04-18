2f9314f5feffba3402a280104274f9868439adda
Tyler Reardon <tdr.reardon@gmail.com>
2018-04-17 23:36:19 -0700

    Can turn on/off tv reliably

--- devices/roku-tv.js
+++ devices/roku-tv.js
@@ -7,7 +7,7 @@ const axios = require('axios')
 class RokuTV extends Device {
   constructor({ id, name, ip }) {
     super({ type: Device.types.tv })
-    this.power({ on: true })
+    this.power({ on: false })
   }
 
   get actions() {
@@ -18,39 +18,66 @@ class RokuTV extends Device {
     }
   }
 
+  wakeup() {
+    return new Promise((resolve, reject) => {
+      wol.wake('1c:1e:e3:df:12:e6'.toUpperCase(), (error) => {
+        if (error) {
+          debug(error)
+          reject(error)
+        } else { resolve() }
+      })
+    })
+  }
+
+  powerKey() {
+    return new Promise((resolve, reject) => {
+      axios.post('http://192.168.0.198:8060/keypress/power').then(response => {
+        resolve(response)
+      }).catch(err => {
+        debug('ERROR', err)
+        reject(err)
+      })
+    })
+  }
+
   power({ on = true } = {}) {
     return new Promise((resolve, reject) => {
-      // this.info().then(info => {
-      //   console.log(info)
-      // }).catch(info => {
-      //   console.log(info)
-      // })
-      if (on) {
-        wol.wake('1c:1e:e3:df:12:e6'.toUpperCase(), (error) => {
-          if (error) {
-            debug(error)
-            reject(error)
-          } else { resolve() }
-        })
-      } else {
-        axios.get('http://192.168.0.198:8060/keypress/power').then(response => {
-          debug(response)
-        }).catch(err => {
-          debug('ERROR', err)
-        })
-      }
+      this.info().then(info => {
+        const powerMode = info['device-info']['power-mode']
+        // If the display is off, and we want to turn on the tv
+        if (on && powerMode !== 'PowerOn') {
+          this.powerKey()
+        } else if (!on && powerMode === 'PowerOn') {
+          // The TV display is on, and we want to turn it off
+          this.powerKey()
+        }
+      }).catch(error => {
+        // The TV is off, so we need to wake it up
+        if (error.timeout && on) {
+          console.log('waking up')
+          this.wakeup()
+        }
+      })
     })
   }
 
   info() {
     return new Promise((resolve, reject) => {
-      axios.post('http://192.168.0.198:8060/query/device-info', { timeout: 2000 }).then((response) => {
+      const source = axios.CancelToken.source()
+      setTimeout(() => {
+        debug('Device is not awake. Cancelling request.')
+        source.cancel()
+        reject({ error: 'Request timed out.', timeout: true })
+      }, 3000)
+      axios.get('http://192.168.0.198:8060/query/device-info', { cancelToken: source.token }).then((response) => {
         try {
           const info = parser.toJson(response.data)
           const obj = JSON.parse(info)
           resolve(obj)
-        } catch (error) { reject(error) }
-      }).catch(error => reject(error))
+        } catch (error) {
+          reject({ error, timeout: false })
+        }
+      }).catch(error => reject({ error, timeout: false }))
     })
   }
 }
