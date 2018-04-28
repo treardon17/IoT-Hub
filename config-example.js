7fe076a37bd84737672fcf954831370e6ebc97a1
Tyler Reardon <tdr.reardon@gmail.com>
2018-04-28 13:50:22 -0700

    Config example

---
+++ config-example.js
@@ -0,0 +1,10 @@
+module.exports = {
+  token: '123456',
+  services: [
+    { name: 'lifx', filename: 'lifx' },
+    { name: 'roku', filename: 'roku' }
+  ],
+  hooks: [
+    { name: 'express', filename: 'server' }
+  ]
+}
