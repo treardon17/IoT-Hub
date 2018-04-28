55b012fffe892836fc2df1ab61f3296adcb6f295
Tyler Reardon <tdr.reardon@gmail.com>
2018-04-28 13:52:27 -0700

    Update gitignore

--- config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  services: [
-    { name: 'lifx', filename: 'lifx' },
-    { name: 'roku', filename: 'roku' }
-  ],
-  hooks: [
-    { name: 'express', filename: 'server' }
-  ]
-}
