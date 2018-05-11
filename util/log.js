7aa7509884642bf99092fdc138014ffc20ca8c2f
Tyler Reardon <tdr.reardon@gmail.com>
2018-05-10 22:11:08 -0700

    Cleaner logging

--- util/log.js
+++ util/log.js
@@ -1,19 +1,18 @@
 const fs = require('fs-extra')
 const path = require('path')
 const FileIO = require('./file-io')
+const stringLength = 22
 
-Log = (name, message, type) => {
+Log = (name, message) => {
   return new Promise((resolve, reject) => {
     const filePath = path.resolve(__dirname, `../logs`)
     const fileName = 'log.txt'
     const fullPath = `${filePath}/${fileName}`
     FileIO.createFilePath({ filePath: fullPath })
       .then(() => {
-        let marker = '*'
-        if (type === 'error') { marker = '!' }
         const date = new Date()
         const dateString = `${date.getMonth()}/${date.getDay()}/${date.getFullYear()}-${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}_${date.getMilliseconds()}ms`
-        const outputString = `${marker}${name}\t\t\t\t${dateString}\t\t\t${message}`
+        const outputString = `${dateString}\t\t| ${name}\t${message}`
         FileIO.appendToFile({ filePath, fileName, output: outputString })
           .then(resolve)
           .catch(reject)
@@ -23,13 +22,16 @@ Log = (name, message, type) => {
 
 Logger = (name) => {
   const debug = require('debug')(name)
-  return (message, type) => {
-    let typeMessage = ''
-    if (type && typeof type === 'string') {
-      typeMessage = `**${type.toUpperCase()}**`
-    }
-    debug(typeMessage, message)
-    Log(name, message, type)
+  let logName = name
+  // If the name is shorter than the length we specified
+  // then make the string that length by appending spaces
+  if (name.length < stringLength) {
+    const difference = stringLength - name.length
+    for(let i = 0; i < difference; i += 1) { logName += ' ' }
+  }
+  return (...message) => {
+    debug(...message)
+    Log(logName, message)
   }
 }
 
