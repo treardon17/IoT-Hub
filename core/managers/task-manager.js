1d8598f5b3ec60bed7edbe1bcb1cde53ef62ebac
Tyler Reardon <tdr.reardon@gmail.com>
2018-05-16 22:13:59 -0700

    Can check the status of tasks to see if they are active

--- core/managers/task-manager.js
+++ core/managers/task-manager.js
@@ -104,8 +104,8 @@ class TaskManager {
       // Keep track of how many lights we're trying to modify
       let completeCount = 0
       const checkResovle = () => {
-        completeCount += 1
         if (completeCount === myDevices.length - 1) { resolve() }
+        completeCount += 1
       }
       let staggerAmt = stagger
       // If we're performing a valid action
@@ -141,8 +141,8 @@ class TaskManager {
         // Keep track of how many lights we're trying to modify
         let completeCount = 0
         const checkResovle = () => {
-          completeCount += 1
           if (completeCount === instructions.length - 1) { resolve() }
+          completeCount += 1
         }
         instructions.forEach(instruction => {
           const { service, action, params } = instruction
@@ -152,6 +152,93 @@ class TaskManager {
           }
           this.performAction({ devices, action, params })
             .then(checkResovle)
+            .catch(reject)
+        })
+      } else {
+        reject({ error: `Invalid task. ${taskID} does not exist in the taskMap` })
+      }
+    })
+  }
+
+  checkStatus({ devices, action, expectedValue }) {
+    return new Promise((resolve, reject) => {
+      let myDevices = devices
+      if (!Array.isArray(devices)) {
+        myDevices = [devices]
+      }
+      // Keep track of how many devices
+      // have the status we're looking for
+      let successCount = 0
+      // Keep track of how many lights we're trying to modify
+      let completeCount = 0
+      const checkResovle = (device, status) => {
+        if (status === expectedValue) { successCount += 1 }
+        if (completeCount === myDevices.length - 1) {
+          resolve({
+            successCount,
+            totalChecked: myDevices.length,
+            success: successCount === myDevices.length
+          })
+        }
+        completeCount += 1
+      }
+      // If we're performing a valid action
+      // Perform that action on every light
+      myDevices.forEach((device) => {
+        debug(`Checking status of ${action} on ${device.id}`)
+        try {
+          const deviceActions = device.actions
+          if (deviceActions[action]) {
+            deviceActions[action]
+              .status()
+              .then((status) => {
+                debug(`${device.name} ${action} status: ${status}`)
+                checkResovle(device, status)
+              })
+              .catch(reject)
+          } else {
+            throw `${action} not found in ${device.name}'s actions`
+          }
+        } catch (error) {
+          debug(error)
+          reject(error)
+        }
+      })
+    })
+  }
+
+  getTaskStatus(taskID) {
+    return new Promise((resolve, reject) => {
+      const task = this.taskMap[taskID]
+      if (task) {
+        const { instructions } = task
+        // Keep track of how many lights we're trying to modify
+        let completeCount = 0
+        const statusArray = []
+        const checkResovle = (status) => {
+          statusArray.push(status)
+          if (completeCount === instructions.length - 1) {
+            let successCount = 0
+            statusArray.forEach(item => {
+              if (item.status.success) { successCount += 1 }
+            })
+            resolve({
+              success: successCount === statusArray.length,
+              statusArray
+            })
+          }
+          completeCount += 1
+        }
+        instructions.forEach(instruction => {
+          const { service, action, params } = instruction
+          let { devices } = instruction
+          if (!devices && service) {
+            devices = this.application.getDevicesOfService(service)
+          }
+          this.checkStatus({ devices, action, expectedValue: params })
+            .then((status) => {
+              checkResovle({ status, action, service })
+            })
             .catch(reject)
         })
       } else {
