8cdef7b26f8f573478aeec5876cdd7deded29e7f
Tyler Reardon <tdr.reardon@gmail.com>
2019-02-14 16:23:30 -0800

    Start main app

--- src/core/app.js
+++ src/core/app.js
@@ -2,7 +2,40 @@ const Observer = require('../util/observer')
 
 class App {
   constructor() {
-    this.services = new Observer()
+    this.setDevices()
+    this.setServices()
+    this.setHooks()
+  }
+
+  // /////////////////
+  // Setup ///////////
+  // /////////////////
+
+  /**
+   * @function setDevices
+   * Creates an observable object containing the different devices in the application.
+   * This object will be populated by the different services loaded in.
+   */
+  setDevices() {
+    this.devices = new Observer({ onSet: this.onModifyDevices.bind(this) })
+  }
+
+  setServices() {
+    this.services = new Observer({ onSet: this.onModifyServices.bind(this) })
+  }
+
+  setHooks() {}
+
+  // /////////////////
+  // Events //////////
+  // /////////////////
+
+  onModifyDevices(params) {
+    console.log('onModifyDevices', params)
+  }
+
+  onModifyServices(params) {
+    console.log('onModifyServices', params)
   }
 }
 
